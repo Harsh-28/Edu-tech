c64653868ef81b6924214bcf6a8c662026fd39f0
Harsh-28 <harshsree677@gmail.com>
2024-03-26 23:33:37 +0530

    Update dataBase.js

--- Server/config/dataBase.js
+++ Server/config/dataBase.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 exports.connect = () => {
-    mongoose.connect(process.env.MONGODB_URL, {
-        userNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    mongoose.connect(process.env.MONGODB_URL)
 
     .then(() => console.log('dataBase Successfully Connected!!'))
     .catch((error) => {
-        console.log("DB connection failed!)");
+        console.log("DB connection failed!");
         console.error(error);
         process.exit(1);
     })
